b738522f9400bc72cff793e397f711aa28f01f7c
Doug Bell <doug@preaction.me>
2017-07-25 00:51:09 -0500

    add bootstrap to make it all pretty

--- lib/MillennialTitle.pm
+++ lib/MillennialTitle.pm
@@ -130,6 +130,23 @@ my @KILLED = keys %KILLED;
 sub startup {
     my ( $app ) = @_;
     push @{ $app->renderer->classes }, __PACKAGE__;
+
+    $app->plugin( 'AssetPack',
+        pipes => [qw< Css JavaScript >],
+    );
+    $app->asset->process(
+        'prereq.css' => (
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.css',
+        ),
+    );
+    $app->asset->process(
+        'prereq.js' => (
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js',
+        ),
+    );
+
     $app->routes->get( '/' )->to( cb => sub {
         my ( $c ) = @_;
         my $killed = $KILLED[ int rand @KILLED ];
@@ -156,42 +173,55 @@ __DATA__
 
 @@ index.html.ep
 <title>Millennial Title Generator</title>
+%= asset 'prereq.css'
+%= asset 'prereq.js'
 <style>
-body {
+h1, h2, h3, h4, h5, h6 {
     text-align: center;
-    max-width: 600px;
-    margin: 0 auto;
 }
 </style>
-<h1>Millennial Title Generator</h1>
-<p>Millennials have been accused of destroying modern civilization. As
-a reward for a job well done, here's an honorific title, and a description of your
-accomplishment along with a link to the news report of your heroic deed. Wear it
-proudly!</p>
+<div class="container">
+    <h1>Millennial Title Generator</h1>
+    <p>Millennials have been accused of destroying modern civilization. As
+    a reward for a job well done, here's an honorific title, and a description of your
+    accomplishment along with a link to the news report of your heroic deed. Wear it
+    proudly!</p>
 
-<h2>You Are...</h2>
-<h3><%= $rank %> <%= $object %></h3>
-<h3><a rel="nofollow" href="<%= $killed_source %>"><%= $action %> of <%= $killed %></a></h3>
-<button onclick="location.reload()">Get another</button>
+    <h2>You Are...</h2>
+    <h3><%= $rank %> <%= $object %></h3>
+    <h3><a rel="nofollow" href="<%= $killed_source %>"><%= $action %> of <%= $killed %></a></h3>
+    <p class="text-center">
+        <button class="btn btn-primary" onclick="location.reload()">Get another</button>
+    </p>
 
-<p>
-    <a href="/list">See all of Millennial's accomplishments</a>
-</p>
+    <p class="text-center">
+        <a href="/list">See all of Millennial's accomplishments</a>
+    </p>
+</div>
 
 @@ list.html.ep
 <title>Millennial Accomplishments</title>
-<h1>Millennial Accomplishments</h1>
-<p>These are all the things millennials have been accused of killing.
-<a href="/">Get a personal title and accomplishment</a>.</p>
+%= asset 'prereq.css'
+%= asset 'prereq.js'
+<style>
+h1, h2, h3, h4, h5, h6 {
+    text-align: center;
+}
+</style>
+<div class="container">
+    <h1>Millennial Accomplishments</h1>
+    <p>These are all the things millennials have been accused of killing.
+    <a href="/">Get a personal title and accomplishment</a>.</p>
 
-<ul>
-    % my %killed = %{ stash 'killed' };
-    % for my $killed ( sort keys %killed ) {
-    <li><%= $killed %>:
-        <a href="<%= $killed{ $killed }[0] %>">
-            <%= $killed{ $killed }[0] %>
-        </a>
-    </li>
-    % }
-</ul>
-
+    <dl>
+        % my %killed = %{ stash 'killed' };
+        % for my $killed ( sort keys %killed ) {
+        <dt><%= $killed %></dt>
+        <dd>
+            <a href="<%= $killed{ $killed }[0] %>">
+                <%= $killed{ $killed }[0] %>
+            </a>
+        </dd>
+        % }
+    </ul>
+</div>
